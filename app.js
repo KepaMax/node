585bb86ac23d060931d35632f6efcd1dd5d44ca5
Kənan Muradov <maljerov.1999@gmail.com>
2024-04-26 14:09:39 +0300

    Tasks update

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const events = require("events");
 const fs = require("fs");
+const http = require("http");
+
+///////////// Task 1
 
 ///////// messaging Event
 // const emitter = new events.EventEmitter();
@@ -36,4 +39,77 @@ const fs = require("fs");
 //     } else {
 //         console.log("File written successfully!");
 //     }
+// });
+
+
+
+////////////// Task 2
+// const readStream = fs.createReadStream("shinobu-rm.jpg")
+// const writeStream = fs.createWriteStream("newPicture.jpg")
+
+
+/////// manual
+// readStream.on("data",(chunk)=>{
+//     console.log("-- NEW CHUNK --")
+//     console.log(chunk)
+//     writeStream.write(chunk)
+// })
+
+/////// with pipe
+// readStream.pipe(writeStream)
+
+
+
+//////////// Task 3
+// const server = http.createServer((req, res) => {
+//     console.log("Request made to server")
+//     res.setHeader("Content-Type", "text/html")
+//     fs.readFile("dummy.htm", (err, data) => {
+//         if (err) {
+//             console.log(err)
+//             res.end();
+//         }
+//         else {
+//             res.write(data);
+//             res.end();
+//         }
+//     })
+// })
+
+// const PORT = 3000
+// server.listen(PORT, "localhost", () => {
+//     console.log("listening on port 3000");
+// })
+
+
+//////////////// Task 4
+// const express = require("express")
+
+// const app = express();
+// app.get("/",(req,res)=>{
+//     console.log("here");
+//     // res.status(200).send("hi");
+//     // res.status(500).json({message:"Server error happened!"});
+//     // res.download("arthurmorgan.jpeg")
+//     //res.sendFile(`${__dirname}/test.html`)
+// })
+// app.get("users/:id",(req,res)=>{
+//     console.log(req.params.id);
+// })
+// app.post("/",(req,res)=>{
+//     res.send("POST request received")
+// })
+// app.put("/",(req,res)=>{
+//     res.send("PUT request received")
+// })
+// app.delete("/",(req,res)=>{
+//     res.send("DELETE request received")
+// })
+// app.use((req,res)=>{
+//     res.status(404).send("404 Not Found")
+// })
+
+
+// app.listen(3000,()=>{
+//     console.log("Server running on 3000")
 // });
